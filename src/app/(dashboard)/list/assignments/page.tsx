7a1da20eeb57ddf066d82779a2564f294b2a636e
matheustheone <matepapava123@gmail.com>
2024-10-03 23:42:38 +0400

    adding recat

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -7,6 +7,7 @@ import {
   role,
 } from "@/lib/data";
 import Image from "next/image";
+import React from "react";
 
 type Assignment = {
   id: number;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 const Homepage = () => {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gradient-to-b from-gray-900 to-gray-600 p-4">
-      <div className="space-y-8 w-full max-w-md gap-9">
-        <a href="/dashboard">
-          <button className="w-full px-6 py-4 bg-black text-white font-semibold rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:bg-gray-800">
+    <main className="flex items-center justify-center min-h-screen bg-white p-4">
+      <div className="space-y-8 w-full max-w-sm">
+        <a href="/admin">
+          <button className="w-full px-6 py-4 bg-black text-white font-semibold rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 hover:bg-gray-800">
             Admin Dashboard
           </button>
         </a>
         <a href="/user">
-          <button className="w-full px-6 py-4 bg-black text-white font-semibold rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:bg-gray-800">
+          <button className="w-full px-6 py-4 bg-black text-white font-semibold rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 hover:bg-gray-800">
             User Dashboard
           </button>
         </a>
         <a href="/company">
-          <button className="w-full px-6 py-4 bg-black text-white font-semibold rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:bg-gray-800">
+          <button className="w-full px-6 py-4 bg-black text-white font-semibold rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 hover:bg-gray-800">
             Company Dashboard
           </button>
         </a>
