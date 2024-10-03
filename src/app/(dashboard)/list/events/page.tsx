10820c2792b50ec1bf856c955147dcbd2fd9f689
matheustheone <matepapava123@gmail.com>
2024-10-03 23:42:47 +0400

    adding reaxt

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -4,6 +4,7 @@ import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { eventsData, role } from "@/lib/data";
 import Image from "next/image";
+import React from "react";
 
 type Event = {
   id: number;
--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -4,6 +4,7 @@ import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { examsData, role } from "@/lib/data";
 import Image from "next/image";
+import React from "react";
 
 type Exam = {
   id: number;
