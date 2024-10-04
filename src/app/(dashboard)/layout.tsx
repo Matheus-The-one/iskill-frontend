68333f4a5b2828197c31b7cef279de7f183f39dc
matheustheone <matepapava123@gmail.com>
2024-10-04 12:51:08 +0400

    adding logo tp the site

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,8 +16,8 @@ export default function DashboardLayout({
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block font-bold">SchooLama</span>
+        
+          <span className="hidden lg:block font-bold text-3xl text-yellow-400"><span className="text-purple-900 text-3xl">I</span>skill</span>
         </Link>
         <Menu />
       </div>
